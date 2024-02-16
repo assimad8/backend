b1fadca4e6b2d350ed90465c1948543dc40960f7
emad <assimad8@gmail.com>
2024-02-16 23:03:08 +0100

    create user serialzer for user model

---
+++ mysite/core/user/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from core.user.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(source='public_id',read_only=True,format='hex')
+    
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id","username","email","password","first_name","last_name"]
+        red_only_field = ["is_active", "is_staff", "is_superuser"]
